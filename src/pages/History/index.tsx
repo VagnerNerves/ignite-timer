c1aefe66f6c9f965c4e064075edc7608b02eb45e
Vagner Nerves <vagnernervessantos@gmail.com>
2023-01-01 11:36:58 -0300

    feat: Created Status colors in History.

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   return (
@@ -20,25 +20,33 @@ export function History() {
               <td>Tarefa bla bla bla</td>
               <td>20 minutos</td>
               <td>Há 2 minutos</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa bla bla bla</td>
               <td>20 minutos</td>
               <td>Há 2 minutos</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa bla bla bla</td>
               <td>20 minutos</td>
               <td>Há 2 minutos</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa bla bla bla</td>
               <td>20 minutos</td>
               <td>Há 2 minutos</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
             </tr>
           </tbody>
         </table>
